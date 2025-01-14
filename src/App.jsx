1b1658899685025e3c563d60e2edd2d948e661f2
diwasbk <diwasbk903@gmail.com>
2025-01-14 11:58:51 +0545

    Learned about React components, JSX, and import/export

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Coffee from './components/Coffee'
 
 function App() {
 
   return (
     <>
+    <h1>This is my React JS learning</h1>
+    <Coffee/>
     </>
   )
 }
---
+++ src/components/Coffee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Coffee() {
+  return (
+    <div>
+        <h2>Coffee with react js</h2>
+        <p>@Happy Coding!</p>
+    </div>
+  )
+}
+
+export default Coffee

b9add9a06fa5b2324d54adf95e6ef1827edd565f
diwasbk <diwasbk903@gmail.com>
2025-01-25 22:01:37 +0545

    Message passed between Sender and Receiver using useNavigate and useLocation

--- src/components/Receiver.jsx
+++ src/components/Receiver.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Custom.css"
+import { useLocation } from "react-router-dom";
 
 function Receiver() {
 
+  //Retrieve data using useLocation
+  const location = useLocation();
+  const receivedMsg = location.state;
+
   return (
-    <div className='sender-receiver-page-container'>
-        <p>I'm Receiver</p>
+    <div className='page-container'>
+        <p>{receivedMsg.message}</p>
     </div>
   )
 }
--- src/components/Sender.jsx
+++ src/components/Sender.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import "./Custom.css"
+import { useNavigate } from 'react-router-dom'
 
 function Sender() {
 
+  const navigate = useNavigate();
+  const msgToSend = {message: "Hello from Sender!"};
+
+  // Navigate to Receiver with state
+  function goToReceiver(){
+    navigate("/receiver", {state:msgToSend});
+  }
+  
   return (
     <>
     <div className='sender-receiver-page-container'>I'm Sender
-      <button className='sender-button'>Send</button>
+      <button className='sender-button' onClick={goToReceiver}>Send</button>
     </div>
     </>
   )
